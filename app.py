8d65f12f389aab7fc30d8f11b0686d826788fca9
ExplorerKut <kutbuddin.ambawala@gmail.com>
2022-02-21 12:18:43 +0530

    added edit functionality and ability to view completed Task

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
+from asyncio.windows_events import NULL
 from flask import Flask,request,render_template,jsonify
 from flask_sqlalchemy import SQLAlchemy
-
-
-
+from sqlalchemy import func
 from sqlalchemy import desc
 app=Flask(__name__)
 app.config['SECRET_KEY']='mysupersecretkey'
 app.config['SQLALCHEMY_DATABASE_URI']='postgresql://nnwhxofswdonpp:5fcdee82cc541c3cdbbdce26371c883a630600754bf82d0583faf1f57c76b289@ec2-54-164-22-242.compute-1.amazonaws.com:5432/d2phg86024slai'
 db=SQLAlchemy(app)
 from models import Task
+from datetime import datetime
 
 # the index route show the home/index page
 @app.route('/')
@@ -21,53 +21,95 @@ def home():
 def addTask():
     #get request data from post request[description, status]
     request_task=request.json   
-    
-    task_details={"id":int(request_task.get("id")),"description":request_task.get("description"),"complete":request_task.get("status")}
-    #create entry in database for given task
-    entry=Task(id=task_details["id"],description=task_details["description"],status=True if task_details["complete"]==1 else False)
-    db.session.add(entry)
-    db.session.commit()
+    #convert date into correct format
+    task_creation_datetime=datetime.strptime(request_task["date"],"%d %b %Y, %H:%M:%S")
 
+    task_details={"id":request_task.get("id"),"description":request_task.get("description"),"complete":request_task.get("status"),"date_created":task_creation_datetime}
+    #check if the task is already present
     
-    #return status
-    return jsonify([{"status":"OK"},{"task":request_task}])
+    # alreadyPresent=Task.query.filter_by(description=task_details["description"]).all()
+    alreadyPresent=Task.query.filter(func.lower(Task.description)==task_details["description"].lower()).all()
+    if len(alreadyPresent)==0:
+        #create entry in database for given task
+        entry=Task(id=task_details["id"],description=task_details["description"],status=True if task_details["complete"]==1 else False,date_created=task_creation_datetime)
+        db.session.add(entry)
+        db.session.commit()
+        #return status and True
+        return jsonify([{"status":"OK"},{"inserted":"True"},{"task":request_task}])
+    else:
+        #return status Ok and False
+        return jsonify([{"status":"OK"},{"inserted":"False"},{"task":request_task}])
 
 
 #route to get tasks which are not completed
 @app.route('/getTasks',methods=['GET'])
-def getTask():
+def getTasks():
     #query database by status, where incomplete tasks are fetched
-    incomplete_task=Task.query.filter_by(status=False).order_by("id").all()
+    incomplete_task=Task.query.filter_by(status=False).order_by("date_created").all()
 
-    send_data=[]
-    for i in incomplete_task:
-        temp={}
-        temp["id"]=i.id
-        temp["description"]=i.description
-        send_data.append(temp)
-    # print(send_data)
-    #return list of task which are incomplete
-    return jsonify([{"status":"OK","tasks":send_data}])
+    complete_task=Task.query.filter_by(status=True).order_by(desc("date_completed")).all()
+
+    # print(len(complete_task))
+    print(len(incomplete_task))
+    #send both the list of complete and incomplete task
+    if len(incomplete_task)>0 and len(complete_task)>0:
+        task_completed=[]
+        task_incomplete=[]
+        for i in incomplete_task:
+            temp={}
+            temp["id"]=i.id
+            temp["description"]=i.description
+            task_incomplete.append(temp)
+        for i in complete_task:
+            temp={}
+            temp["id"]=i.id
+            temp["description"]=i.description
+            task_completed.append(temp)
+        return jsonify([{"status":"OK","completed":"False","incompleteTasks":task_incomplete,"completeTask":task_completed}])
+    #if incompleteTask are zero and complete task are more
+    elif len(incomplete_task)<=0 and len(complete_task)>0:
+        # print("HEre")
+        task_completed=[]
+        for i in complete_task:
+            temp={}
+            temp["id"]=i.id
+            temp["description"]=i.description
+            task_completed.append(temp)
+        return jsonify([{"status":"OK","completed":"True","incompleteTasks":None,"completeTask":task_completed}])
+    # if completeTask are not zero and incomplete task are zero
+    elif len(incomplete_task)>0 and len(complete_task)<=0:
+        task_incomplete=[]
+        for i in incomplete_task:
+            temp={}
+            temp["id"]=i.id
+            temp["description"]=i.description
+            task_incomplete.append(temp)
+        return jsonify([{"status":"OK","completed":"False","incompleteTasks":task_incomplete,"completeTask":None}])
+    #if no complete and incomplete task left
+    else:
+        return jsonify([{"status":"OK","completed":"True","incompleteTasks":None,"completeTask":None}])
     
 
 
 @app.route("/updateStatus",methods=["POST"])
-def updateTasks():
+def updateStatus():
     #get description whose status needs to be changed
     request_params=request.json;
-    print(request_params)
+    # print(request_params)
     #get task by id
-    data=Task.query.filter_by(id=request_params).first()
-
-    # check if the task is completed or not and set accordingly
+    data=Task.query.filter_by(id=request_params.get("value")).first()
+    date_completed=datetime.strptime(request_params["updatedDate"],"%d %b %Y, %H:%M:%S")
+    # check if the task is completed or not and set accordingly the completed date
     if data.status==True:
         data.status=False
+        data.date_completed=None
     else:
         data.status=True
+        data.date_completed=date_completed
 
     #commit the changes
     db.session.commit()
-    return jsonify([{"status":"OK"}])
+    return jsonify([{"status":"OK","completed":data.status}])
 
 
 
@@ -76,12 +118,27 @@ def deleteTasks():
     #delete unwanted tasks
     request_params=request.json;
     #delete tasks with specific id
-    data_delete=Task.query.filter_by(id=int(request_params)).first()
+    data_delete=Task.query.filter_by(id=request_params).first()
     db.session.delete(data_delete)
     #commit changes
     db.session.commit()
 
-    return jsonify({"status":"OK"})
+    return jsonify({"status":"OK","deleted":True})
+
+@app.route("/updateTask",methods=["POST"])
+def updateTask():
+    #edit task
+    request_params=request.json;
+    #get task by id which need to be updated
+    row_update=Task.query.filter_by(id=request_params.get("id")).first()
+    #if any change in description then update else return False
+    if request_params.get("description")!=row_update.description:
+        row_update.description=request_params.get("description")
+        updated_description=row_update.description
+        db.session.commit()
+        return jsonify({"status":"OK","updated":"True","tasks":updated_description})
+    else:
+        return jsonify({"status":"OK","updated":"False","tasks":request_params.get("description")})
 
 if __name__=="__main__":
     app.run(debug=True)
--- static/index.js
+++ static/index.js
@@ -2,50 +2,76 @@
         var list=document.querySelector('.todolist');
         let lastId=0;
         list.addEventListener('click',function(ev){
-            if(ev.target.tagName==='SPAN'){
-                if(ev.target.classList.toggle('checked')){
-                    ev.target.closest('div').querySelector('INPUT').checked=true;
-                }
-                else{
-                    ev.target.closest('div').querySelector('INPUT').checked=false;
-                    
-                }
-                //send request to change the status
-                sendCompletedList(ev.target.id);
-            }
-            if(ev.target.tagName==='INPUT'){
+            //if checkbox is selected then strikeout text and send request
+            if(ev.target.tagName==='INPUT'&&ev.target.type==="checkbox"){
 
                 if(ev.target.checked==true){
-                    ev.target.closest('div').parentElement.querySelector('SPAN').classList.toggle('checked');
+                    ev.target.closest('div').parentElement.querySelector('INPUT').classList.toggle('checked');
                 }
                 else{
-                    ev.target.closest('div').parentElement.querySelector('SPAN').classList.toggle('checked');
+                    ev.target.closest('div').parentElement.querySelector('INPUT').classList.toggle('checked');
                     
                 }
                 //send request to change the status
-                sendCompletedList(ev.target.closest('div').parentElement.querySelector('SPAN').id);
+                sendCompletedList(ev.target.closest('div').parentElement.querySelector('INPUT').id);
             }
-
+            //if button is clicked let user change the description of task
+            if(ev.target.tagName==='BUTTON'&&ev.target.id=='edit'){
+                if(ev.target.closest('div').querySelector('INPUT').checked==true){
+                    window.alert("Cannot Edit completed task, Uncheck Task first");
+                }
+                else{
+                    // window.alert("here");
+                    console.log(ev.target.closest('div').querySelector('INPUT'));
+                    ev.target.closest('div').parentElement.querySelector('INPUT[type="text"]').readOnly=false;
+                    ev.target.closest('div').parentElement.querySelector('INPUT[type="text"]').focus();
+                }
+            }
         },false);
-        //when the delete button is clicked delete the div from html and send a request to function for deleting in csv
+        //when the delete button is clicked delete the div from html and send a request to function for deleting in database
         let taskDelete=document.querySelector('.todolist');
         taskDelete.addEventListener('click',async function(ev){
-            if(ev.target.tagName==='BUTTON'){
-                //delete from csv wait for confirmation from database
-                let status=await deleteTask(ev.target.closest('div').parentElement.querySelector('span').id);
+            if(ev.target.tagName==='BUTTON'&&ev.target.id==='delete'){
+                //delete from site wait for confirmation from database
+                let status=await deleteTask(ev.target.closest('div').parentElement.querySelector('input').id);
                 if(status["status"]=="OK"){
                     ev.target.closest('div').parentElement.remove();
                 }
-                
+            }
+
+        },false);
+        //delete completed list task
+        let deleteCompletedTask=document.querySelector('.completed-list');
+        deleteCompletedTask.addEventListener('click',async function(ev){
+            if(ev.target.tagName==='BUTTON'){
+                //delete from csv wait for confirmation from database
+                let status=await deleteTask(ev.target.closest('div').querySelector('SPAN').id);
+                if(status["status"]=="OK"){
+                    ev.target.closest('div').remove();
+                }
             }
 
         },false);
     
-        //post request for updating status in csv
+        //post request for updating status in database
         function sendCompletedList(val){
+            let date= new Date();
+            const formattedDate = date.toLocaleString("en-GB", {
+                day: "numeric",
+                month: "short",
+                year: "numeric",
+                hour: "numeric",
+                minute: "2-digit",
+                second:"2-digit"
+              });
+            const send_data={
+                value:val,
+                updatedDate:formattedDate
+
+            }
            fetch('/updateStatus',{
                 method:'POST',
-                body:JSON.stringify(val),
+                body:JSON.stringify(send_data),
                 headers: {
                     "Content-type": "application/json; charset=UTF-8",
                 }
@@ -54,7 +80,7 @@
            .then(response=>response.json())
         
         }
-        //post request for deleting task from csv
+        //post request for deleting task from database
         function deleteTask(val){
             console.log(val);
            return fetch('/deleteTask',{
@@ -67,47 +93,115 @@
            })
            .then(response=>response.json())
         }
+        //insert the task after getting confirmation from database
+        function insertTaskComplete(item,taskDetails){
+            // let parentDiv=document.getElementById('completed-list');
+            let headerCompleted=document.createElement('h3');
+            headerCompleted.innerHTML="Completed tasks";
+            taskDetails.appendChild(headerCompleted)
+            let divElement=document.createElement('div');
+            divElement.className="task-list"
 
+            let spanElement=document.createElement('span');
+            spanElement.id=item['id'];
+            spanElement.innerHTML=item['description']
+            let buttonElement=document.createElement('button');
+            buttonElement.innerHTML='x';
 
-
-        //function to get incomplete task from csv
-        function getTasks(){
-                    var divElement;
-                    var TaskDiv;
-            fetch("/getTasks")
-            .then(response=>response.json())
-            .then(json=>{
-                const taskDetails = document.getElementById("todolist");   
-                console.log(json);
-                if(json[0]["status"]==="OK"){ 
-                let data_tasks=json[0]["tasks"];
-                data_tasks.forEach(item=>{
-                
-                    //for each item we get from the csv create a div and append span,button, checkbox to it.
+            // buttonElement.onclick=deleteTask(item['id']);
+            divElement.appendChild(spanElement);
+            divElement.appendChild(buttonElement);
+            
+            taskDetails.appendChild(divElement);
+            
+            
+        }
+        //insert the completed task aftergetting confirmation from database
+        function insertTaskIncomplete(item, taskDetails){
+            if(item["incompleteTasks"]!="None"){
+                    //for each item we get from the database create a div and append span,button, checkbox to it.
                     divElement = document.createElement('div');
                     TaskDiv = document.createElement('div');
                     divElement.className = "task";
                     TaskDiv.className="taskName";
-                    let spanElement = document.createElement('span');
-                    spanElement.innerHTML = item["description"]
-                    spanElement.id=item["id"]
+
+                    let inputTask=document.createElement('input');
+                    inputTask.type="text";
+                    inputTask.id=item["id"];
+                    inputTask.onkeyup=updateTask;
+                    inputTask.readOnly=true;
+                    inputTask.value=item["description"];
                     let buttonElement = document.createElement('button');
+                    let EditElement=document.createElement('button');
                     let checkElement=document.createElement('input');
                     checkElement.type="checkbox";
                     buttonElement.innerHTML = 'x';
-                    divElement.appendChild(spanElement);
+                    EditElement.id='edit';
+                    EditElement.innerHTML="EDIT";
+                    buttonElement.id='delete';
+                    divElement.appendChild(inputTask);
+                    
                     TaskDiv.appendChild(checkElement);
+                    TaskDiv.appendChild(EditElement);
                     TaskDiv.appendChild(buttonElement);
+                    
                     divElement.appendChild(TaskDiv);
                     taskDetails.appendChild(divElement);
-                    lastId=item["id"];
-                    // console.log(lastId)
+        }
+    }
+
+
+        //function to get incomplete task from database
+        function getTasks(){
+                    let divElement;
+                    let TaskDiv;
+            fetch("/getTasks")
+            .then(response=>response.json())
+            .then(json=>{
+                   
+                // console.log(json);
+                //if status is ok and no incomplete task left
+                if(json[0]["status"]==="OK"&&json[0]["completed"]=="True"){
                     
-                })
+                    //if no task is added
+                    if(json[0]["completeTask"]==null&&json[0]["incompleteTasks"]==null){
+                        window.alert("No task added");
+                    }
+                    //if every task is completed
+                    if(json[0]["completeTask"]!=null){
+                        let taskDetails=document.getElementById("completed-list")
+                        let data_tasks=json[0]["completeTask"];
+                        data_tasks.forEach(item=>{
+                        insertTaskComplete(item,taskDetails)
+                        })
+                        window.alert("You completed every task");
+                        
+                    } 
+                }
+                //if there are incomplete task left
+                if(json[0]["status"]==="OK" && json[0]["completed"]=="False"){
+
+                    const taskDetails = document.getElementById("todolist");
+                    let data_tasks=json[0]["incompleteTasks"];
+                    data_tasks.forEach(item=>{
+                    insertTaskIncomplete(item,taskDetails)
+                    
+                }
+                )
+                //if some task are completed
+                if(json[0]["completeTask"]!=null){
+                    let taskDetails=document.getElementById("completed-list")
+                    let data_tasks=json[0]["completeTask"];
+                    data_tasks.forEach(item=>{
+                    insertTaskComplete(item,taskDetails)
+                    })
+
+                    
+                }
             }
             })
             .catch((error)=>{
-                console.error('Error:'.error)
+                console.error(error);
             });
         }
         //add task on enter key press
@@ -116,51 +210,88 @@
                 addToDo();
             }
         }
+        //post request to send the updated task description
+        function updateTask(e){
+            if(e.which==13||e.keyCode==13){
+                const data={
+                    id:e.target.id,
+                    description:e.target.value
+                };
+            fetch("/updateTask",{
+                method:"POST",
+                body:JSON.stringify(data),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            }).then(response=>response.json())
+            .then(json=>{
+                e.target.readOnly=true;
+                e.target.blur();
+                console.log(json);
+            })
+                
+            }
+        }
         //add task when clicked on button
         async function addToDo() {
             if(document.getElementById("description").value!='')
-            {const data = {
+            {   
+                let date=new Date();
+                const formattedDate = date.toLocaleString("en-GB", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                    hour: "numeric",
+                    minute: "2-digit",
+                    second:"2-digit"
+                  });
+                const data = {
                 description: document.getElementById("description").value,
-                id:lastId+1,
-                status: 0
+                id:uuidv4(),
+                status: 0,
+                date:formattedDate,
             };
-            lastId=lastId+1;
+            console.log(data);
             // console.log(data["id"]);
-            let checkResponse=await addDataToCsv(data);
-            if(checkResponse[0]["status"]==="OK"){
-            // console.log(data);
+            let checkResponse=await addDataToDatabase(data);
+            if(checkResponse[0]["status"]==="OK"&&checkResponse[1]["inserted"]==="True"){
+            console.log(checkResponse);
             let divElement=document.createElement('div');
             let taskDiv=document.createElement('div');
             taskDiv.className="taskName";
             divElement.className="task";
-            let spanElement=document.createElement('span');
-            spanElement.innerHTML=data["description"];
-            spanElement.id=lastId;
+            let inputTask=document.createElement('input');
+            inputTask.type="text";
+            inputTask.id=data["id"];
+            inputTask.onkeyup=updateTask;
+            inputTask.readOnly=true;
+            inputTask.value=data["description"];
             let buttonElement=document.createElement('button');
+            let EditElement=document.createElement('button');
             let checkElement=document.createElement('input');
             checkElement.type="checkbox";
-            // checkElement.id="taskComplete"
+            EditElement.innerHTML="EDIT";
+            EditElement.id="edit";
+            buttonElement.id='delete';
             buttonElement.innerHTML='x';
             const taskDetails = document.getElementById("todolist");
-            
-            divElement.appendChild(spanElement);
+            divElement.appendChild(inputTask);
             taskDiv.appendChild(checkElement);
+            taskDiv.appendChild(EditElement);
+
             taskDiv.appendChild(buttonElement);
             divElement.appendChild(taskDiv);
             taskDetails.appendChild(divElement);
             }
+            else{
+                window.alert("Task already present");
+            }
             
         }
-        //send data to csv as post request
-        function addDataToCsv(data){
-            // const data = {
-            //     description: document.getElementById("description").value,
-            //     id:lastId+1,
-            //     status: 0
-            // };
-            // let responseSend={};
+        //send data to database as post request
+        function addDataToDatabase(data){
             document.getElementById("description").value='';
-            let send_data=JSON.stringify({id:data["id"],description:data["description"],status:0})
+            let send_data=JSON.stringify(data)
             // console.log(send_data)
            return fetch("/add-todo",{
                 method:"POST",
